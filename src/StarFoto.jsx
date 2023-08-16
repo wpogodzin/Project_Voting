aabbf70431b78729ec03de499651221fe1bd4606
Vitaliy Pogodin <pogodin.vitaliy.s@gmail.com>
2023-08-16 21:56:51 +0200

    fotos in src/StarFoto.jsx are located remotely;in case of locating locally fotos are invisible on netlify.com

--- src/StarFoto.jsx
+++ src/StarFoto.jsx
@@ -4,22 +4,22 @@
   const star_array = [
     {
     name: 'Maradona',
-    imageUrl: '/src/components/maradona.jpeg',
+    imageUrl: 'https://media.gettyimages.com/id/1363115696/photo/argentina-captain-diego-maradona-holds-aloft-the-trophy-after-the-fifa-1986-world-cup-final.jpg?s=612x612&w=0&k=20&c=N6BIF3hmsAHPqSUKYH8S3RjPjYTeMbjbvuHujT88axQ=',
     imageSize: 190
     },
       {
       name: 'Messi',
-      imageUrl: 'src/components/messi.jpeg',
+      imageUrl: 'https://pbs.twimg.com/media/FkR-TZAXwAApp1C.jpg:large',
       imageSize: 190
       },
     {
     name: 'Pele',
-    imageUrl: 'src/components/pele.jpg',
-    imageSize: 190 
+    imageUrl: 'https://media.gettyimages.com/id/96522194/photo/brazilian-football-star-pele-kisses-the-fifa-world-cup-during-its-presentation-in-rio-de.jpg?s=612x612&w=gi&k=20&c=V7GWHDk2HzAHXurQo4l0icEpJtljkDiK1iTridpzxAI=',
+    imageSize: 190
     },
       {
       name: 'Ronaldo',
-      imageUrl: 'src/components/ronaldo.jpg',
+      imageUrl: 'https://c8.alamy.com/comp/HE62J7/yokohama-18th-dec-2016-real-madrid-forward-cristiano-ronaldo-kisses-HE62J7.jpg',
       imageSize: 190       
       }
   ];
@@ -31,7 +31,7 @@
       <>
         <img
           className="avatar"
-          src={star_array[star_index].imageUrl}  
+          src={star_array[star_index].imageUrl} 
           alt={'Photo of ' + star_array[star_index].name}
           style={{
             width: star_array[star_index].imageSize,
@@ -42,4 +42,4 @@
       </>
     );
   }
-    
+ 
